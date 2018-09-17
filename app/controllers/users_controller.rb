e5a14d73a461916b51005e207c678b3c3b2fe812
Adam Oliveras <theadamoliveras@gmail.com>
2018-09-17 11:35:13 -0700

    connected login link to new user page with new user form, added code for has secure password and such

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,25 @@ class UsersController < ApplicationController
     end
 
     def new
-        #new user form will go here
-    end
+        # Provide the model instance to the form_for helper
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          flash[:notice] = "You have successfully signed up!"
+          redirect_to root_path
+        else
+          render :new
+        end
+      end
+    
+    private
+    
+        # Implement Strong Params
+        def user_params
+          params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
+
 end
