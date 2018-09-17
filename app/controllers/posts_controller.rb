c0d93423ffae2313174d61c32ddfb54532eaee68
Adam Oliveras <theadamoliveras@gmail.com>
2018-09-17 16:38:22 -0700

    created new post form and connected it to controller with methods to make it possible to create and save to the database and then send you back to the post index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,32 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+          flash[:success] = "Object successfully created"
+          redirect_to posts_path
+        else
+          flash[:error] = "Something went wrong"
+          redirect_to 'new'
+        end
+    end
+
+    private
+        def post_params
+            params.require(:post).permit(:title, :body, :dream_time, :dream_type)
+        end
+
+
+
 end
