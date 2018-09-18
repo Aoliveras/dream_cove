e0dc3b4b3ff54fb05be66dea77011ccfdcf3be81
Adam Oliveras <theadamoliveras@gmail.com>
2018-09-17 18:36:52 -0700

    moved links to proper space in the show page, edit and delete links both work properly, put post form inside of partial folder along with navbar and made sure they were in the proper places

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,34 @@ class PostsController < ApplicationController
         end
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to post_path(@post)
+          flash[:success] = "Object was successfully updated"
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        if @post.destroy
+            flash[:success] = 'Object was successfully deleted.'
+            redirect_to posts_path
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to posts_path
+        end
+    end
+    
+
+
     private
         def post_params
             params.require(:post).permit(:title, :body, :dream_time, :dream_type)
