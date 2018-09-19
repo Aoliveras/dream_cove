c705b08146bd55785cb093a5235dc4e075756494
Adam Oliveras <theadamoliveras@gmail.com>
2018-09-18 17:03:32 -0700

    created comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     # belongs_to :user, class_name: "user", foreign_key: "user_id"
     belongs_to :user
+    has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     # Verify that an email exists and that it does not already exist in the db
     validates :email, presence: true, uniqueness: true
     has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
 end
