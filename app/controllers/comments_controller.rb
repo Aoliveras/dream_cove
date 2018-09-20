330b408c70312617a15c0be576874947378b9c60
Adam Oliveras <theadamoliveras@gmail.com>
2018-09-20 13:20:22 -0700

    removed link to delete comments until the code is working for it

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,25 +4,16 @@ class CommentsController < ApplicationController
         @post = Post.find(params[:post_id])
         @comment = @post.comments.new(comment_params)
         @comment.user_id = current_user.id
-        # @comment.user_id = Post.find(params[:user_id])
         @comment.save
         redirect_to post_path(@post)
     end
 
-    # def create
-    #     @comment = current_user.posts.comments.new(comment_params)
-    #     if @comment.save
-    #       flash[:success] = "Object successfully created"
-    #       redirect_to post_path(@post)
-    #     else
-    #       flash[:error] = "Something went wrong"
-    #       redirect_to 'new'
-    #     end
+    # def destroy
+    #     @post = Post.find(params[:post_id])
+    #     @comment = @post.comments.find(params[:id])
+    #     @comment.destroy
+    #     redirect_to post_path(@post)
     # end
-
-    def destroy
-
-    end
 
     def comment_params
         params.require(:comment).permit(:user_name, :title, :body)
